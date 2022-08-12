fcb2f721e463fd5fa52dedfbf583364e31b60772
Nafiul Islam <nafiul.islam@synesisit.info>
2022-08-12 16:34:30 +0600

    env setup

---
+++ src/main/java/Joining_table_spring_boot/JoiningTableSpringBootApplication.java
@@ -0,0 +1,13 @@
+package Joining_table_spring_boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JoiningTableSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JoiningTableSpringBootApplication.class, args);
+	}
+
+}
